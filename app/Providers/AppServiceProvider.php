eb8239bb2e4c317e9b9704a7f461ea94b08a8298
Victor8Danny <59888810+Victor8Danny@users.noreply.github.com>
2023-07-11 14:35:13 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+        URL::forceScheme('https');
         //Schema::enableForeignKeyConstraints();
        
         
