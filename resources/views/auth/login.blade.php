41c244350d84747700fd613bd653c75b44431f38
Victor8Danny <59888810+Victor8Danny@users.noreply.github.com>
2023-07-11 15:13:41 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,36 +18,7 @@
 </head>
 <body style="background-color:#BDC3C7;">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color:#278747;">
-            <div class="container" >
-                <a class="navbar-brand text-light" href="{{ url('/') }}" style="background-color:#278747;">
-                Sistema de control de sssasistensias
-                </a>
-                
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right" >
-                    @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" style="color:#FF0000;">Home</a>
-                    @else
-                        
-
-                    @endauth
-                </div>
-            
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        
 
         <main class="py-1">
         <div class="container">
