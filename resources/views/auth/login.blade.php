4e92600094b71e5eeb104c66d9f1b84ce8c7bb01
Victor8Danny <59888810+Victor8Danny@users.noreply.github.com>
2023-07-12 10:32:59 -0500

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
     <!-- Scripts -->
    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body style="background-color:#BDC3C7;">
+<body style="background-color:#278747;">
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light  shadow-sm" style="background-color:#278747;">
             <div class="container" >
