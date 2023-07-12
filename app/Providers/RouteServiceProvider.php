faad6ee721c492b33c24a5ab366f810f6cd628e5
Victor8Danny <59888810+Victor8Danny@users.noreply.github.com>
2023-07-11 21:51:51 -0500

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/estudiante';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
